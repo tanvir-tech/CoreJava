885c94f6e458d838d7b54e50c7fc13c02025d771
tanvir-tech <tanvirah325@gmail.com>
2021-02-10 12:54:50 +0600

    Insertion_Sort and Merge_Sort practice

--- src/preOJ/demo.java
+++ src/preOJ/demo.java
@@ -2,6 +2,9 @@ package preOJ;
 
 public class demo {
     public static void main(String[] args) {
-        System.out.println("Online judge running");
+        System.out.println("Online judge Running.......... Result=");
+        int res = 5+2;
+        System.out.println(res);
+
     }
 }
