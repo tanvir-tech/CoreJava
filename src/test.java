1b80136e0e09daf8ea33ff7225603d69999e31b9
tanvir ahmed <tanvirah325@gmail.com>
2021-02-12 00:48:15 +0600

    StringIndexOutofBound Exception

--- src/test.java
+++ src/test.java
@@ -1,5 +1,15 @@
 public class test {
     public static void main(String[] args) {
         System.out.println("Java is back again.Don't worry tanvir!");
+
+
+        String message = "Hello world!";                    // 11 is last index
+        String newMessage = message.substring(6, 12)        // 12 excluded
+                + message.substring(12, 6);                 // 12 included
+        System.out.println(newMessage);
+
+
+
+
     }
 }
