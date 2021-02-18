78b150484985aa70eb4e6dd3f43236f7e61540d5
tanvir-tech <tanvirah325@gmail.com>
2021-02-19 00:44:27 +0600

    Maven and JDBC

---
+++ src/main/java/Database_Practice/Demo.java
@@ -0,0 +1,31 @@
+package Database_Practice;
+
+import java.sql.*;
+
+public class Demo {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+
+        String urlPreset = "jdbc:mysql://localhost:3306/";
+        String dataBaseName = "jdbctest";
+        String url = urlPreset + dataBaseName;
+
+        String userName = "root";
+        String passWord = "";
+
+        String Query = "SELECT * FROM student";
+
+        Class.forName("com.mysql.jdbc.Driver");
+        Connection connection = DriverManager.getConnection(url,userName,passWord);
+        Statement statement = connection.createStatement();
+
+        ResultSet resultSet = statement.executeQuery(Query);
+
+        resultSet.next();
+        String name = resultSet.getString("name");
+        System.out.println(name);
+
+        statement.close();
+        connection.close();
+
+    }
+}
